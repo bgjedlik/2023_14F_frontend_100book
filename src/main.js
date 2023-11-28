8c90fa234ef337e21658f726f297a52f7a0cd084
bgjedlik <bolya.gabor@jedlik.eu>
2023-11-28 09:38:06 +0100

    scss

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap'
+import './assets/style.scss'
+
 const app = createApp(App)
 
 app.use(router)
